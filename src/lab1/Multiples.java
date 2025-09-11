564f689ad2a1dafa6c2914c08fc0df3c2b1ea3ea
lscccc12 <lisicheng040511@hotmail.com>
2025-09-11 15:54:00 -0400

    make mehtod public

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -7,7 +7,8 @@ public class Multiples
         System.out.println(multiples());
     }
 
-    static int multiples(int n, int a, int b) {
+    public static int multiples(int n, int a, int b)
+    {
         int number = 0;
         for (int i = 1; i < n; i++) {
             if (i % a == 0 || i % b == 0) {
@@ -16,7 +17,8 @@ public class Multiples
         }
         return number;
     }
-    static int multiples(){
+    public static int multiples()
+    {
         return multiples(1000, 3, 5);
     }
 }
