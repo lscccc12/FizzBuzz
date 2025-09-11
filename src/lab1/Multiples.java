6cd911dcc84a1116c6035bb1be9e7ade40ce4d8c
lscccc12 <lisicheng040511@hotmail.com>
2025-09-11 15:32:51 -0400

    fix the issues when run the test

---
+++ src/lab1/Multiples.java
@@ -0,0 +1,22 @@
+package lab1;
+
+public class Multiples
+{
+    public static void main(String[] args)
+    {
+        int n = 1000;
+        int a = 3;
+        int b = 5;
+        System.out.println(multiples(n, a, b));
+    }
+
+    static int multiples(int n, int a, int b) {
+        int number = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % a == 0 || i % b == 0) {
+                number += 1;
+            }
+        }
+        return number;
+    }
+}
